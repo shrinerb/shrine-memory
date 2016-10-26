4f08d37d3acdf129f293ff5c87bda08153e9e867
Janko Marohnić <janko.marohnic@gmail.com>
2016-10-26 15:48:18 +0800

    Remove unneeded setup block

--- test/memory_test.rb
+++ test/memory_test.rb
@@ -4,9 +4,6 @@ require "shrine/storage/linter"
 describe Shrine::Storage::Memory do
   before do
     @memory = Shrine::Storage::Memory.new
-    shrine = Class.new(Shrine)
-    shrine.storages[:memory] = @memory
-    @uploader = shrine.new(:memory)
   end
 
   it "passes the linter" do
